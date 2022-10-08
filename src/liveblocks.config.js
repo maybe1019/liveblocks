eb020f98f4837889c1080c5a9491e73b9c0d9370
unknown <maybex66@gmail.com>
2022-10-07 21:52:51 -0400

    .

--- src/liveblocks.config.js
+++ src/liveblocks.config.js
@@ -1,15 +1,41 @@
 import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
+let PUBLIC_KEY = "pk_dev_9kBhAAOtYoGvC5yJTv-pSVW_";
+
+if (!/^pk_(live|test)/.test(PUBLIC_KEY)) {
+  console.warn(
+    `Replace "${PUBLIC_KEY}" by your public key from https://liveblocks.io/dashboard/apikeys.\n` +
+      `Learn more: https://github.com/liveblocks/liveblocks/tree/main/examples/react-dashboard#getting-started.`
+  );
+}
+
+overrideApiKey();
+
 const client = createClient({
-  publicApiKey: 'pk_dev_2oNnzRUQH41cWXRBsu_pP76g',
-  secretApiKey: "sk_dev_5p8j83FF4Zq67cypmRzGmxRP",
-  authEndpoint: "/api/auth"
+  publicApiKey: PUBLIC_KEY,
 });
 
 export const {
   suspense: {
+    useHistory,
+    useOthers,
+    useStorage,
+    useMutation,
     RoomProvider,
-    useOthers
-  },
+    useSelf,
+  }
 } = createRoomContext(client);
+
+/**
+ * This function is used when deploying an example on liveblocks.io.
+ * You can ignore it completely if you run the example locally.
+ */
+function overrideApiKey() {
+  const query = new URLSearchParams(window?.location?.search);
+  const apiKey = query.get("apiKey");
+
+  if (apiKey) {
+    PUBLIC_KEY = apiKey;
+  }
+}
